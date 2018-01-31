be11277c8e286582d532fca7fea7591a0bc0d136
Thomas Sutton <me@thomas-sutton.id.au>
2018-01-31 19:55:24 +1100

    Test boundary . boundary == 0

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,31 @@
 {-# LANGUAGE DataKinds        #-}
+{-# LANGUAGE TemplateHaskell  #-}
 {-# LANGUAGE TypeApplications #-}
 module Main where
 
+import           Control.Monad
+import qualified Data.Vector    as V
+import           Hedgehog
+import qualified Hedgehog.Gen   as Gen
+import qualified Hedgehog.Range as Range
+import           System.Exit
+
 import           Lib
 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+genSimplex :: Gen (Simplex 4)
+genSimplex = Simplex . V.fromList <$> Gen.list (Range.linear 1 50) (Gen.word64 (Range.linear 0 100))
 
 -- | It is required (and true) that the boundary of a boundary is empty.
-boundOfBoundIsEmpty :: Simplex 4 -> Bool
-boundOfBoundIsEmpty simplex =
-  0 == chainLength (boundary (boundary (SimplexC @ Z2 simplex)))
+prop_boundOfBoundIsEmpty :: Property
+prop_boundOfBoundIsEmpty = property $ do
+  simplex <- forAll genSimplex
+  0 === chainLength (boundary (boundary (SimplexC @ Z2 simplex)))
+
+tests :: IO Bool
+tests =
+  checkParallel $$(discover)
+
+main :: IO ()
+main = do
+  r <- tests
+  unless r $ exitFailure
